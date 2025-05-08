5d88894633f2d8b8ce6a1a508d432fdf58a26450
Mrdaniel7 <156192906+Mrdaniel7@users.noreply.github.com>
2025-05-08 16:12:01 +0200

    Update environment.prod.ts

--- Front-End/src/environments/environment.prod.ts
+++ Front-End/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080' // <-- Configurado para Back-End en el MISMO contenedor en puerto 8080
+  apiBaseUrl: 'http://localhost:9096' // <-- Configurado para Back-End en el MISMO contenedor en puerto 8080
 };

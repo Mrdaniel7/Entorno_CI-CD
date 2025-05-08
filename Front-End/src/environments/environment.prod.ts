67cf1b79c1ad5a8ecd96b980d6d67f628bdcedf2
Mrdaniel7 <156192906+Mrdaniel7@users.noreply.github.com>
2025-05-08 12:40:49 +0200

    Update environment.prod.ts para que front se conecte con el back

--- Front-End/src/environments/environment.prod.ts
+++ Front-End/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'http://localhost:8080' // <-- Configurado para Back-End en el MISMO contenedor en puerto 8080
 };

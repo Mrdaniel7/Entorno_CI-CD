c1563382a72c56c028c7e23dcb3b80e161bc66af
Mrdaniel7 <156192906+Mrdaniel7@users.noreply.github.com>
2025-05-08 12:46:14 +0200

    Update home.component.ts
    añado loding que es una valiable que hacia falta

--- Front-End/src/app/home/home.component.ts
+++ Front-End/src/app/home/home.component.ts
@@ -6,13 +6,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public currentUser;
+  public loading: boolean = false; // <-- ¡Añade esta línea para definir la propiedad 'loading'!
+
   constructor() {
     this.currentUser = localStorage.getItem('currentUser')? JSON.parse(localStorage.getItem('currentUser')) : '';
-   }
+  }
 
   ngOnInit() {
-
-
   }
 
 }

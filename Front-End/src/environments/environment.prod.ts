d804bc0ef3920df6ab2659f74636a6ba5347b143
Mrdaniel7 <156192906+Mrdaniel7@users.noreply.github.com>
2025-05-19 16:18:40 +0200

    Update environment.prod.ts

--- Front-End/src/environments/environment.prod.ts
+++ Front-End/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  #apiBaseUrl: 'http://host.docker.internal:8082/api'
   apiBaseUrl: 'http://localhost:9096' // <-- Configurado para Back-End en el MISMO contenedor en puerto 8080
 };

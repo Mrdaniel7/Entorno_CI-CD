276bec07629976489a7eb89dbd57a56be03a34a7
Mrdaniel7 <156192906+Mrdaniel7@users.noreply.github.com>
2025-05-20 16:59:16 +0200

    Update interceptor.service.ts

--- Front-End/src/app/_services/interceptor.service.ts
+++ Front-End/src/app/_services/interceptor.service.ts
@@ -30,17 +30,17 @@ export class InterceptorService implements HttpInterceptor {
 
     // Only prepend apiBaseUrl if the request URL is relative
      if (/^https?:\/\//i.test(req.url)) {
-    // Absolute URL, don't modify it
-    clonedReq = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` }
-    });
-  } else {
-    // Relative URL, prepend the API base
-    clonedReq = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` },
-      url: environment.apiBaseUrl + req.url
-    });
-  }
+      // Absolute URL, don't modify it
+      clonedReq = req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` }
+      });
+    } else {
+      // Relative URL, prepend the API base
+      clonedReq = req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` },
+        url: environment.apiBaseUrl + req.url
+      });
+    }
 
     return next.handle(clonedReq).pipe(
       catchError((error: HttpErrorResponse) => {
